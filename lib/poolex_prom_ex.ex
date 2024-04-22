b0762158fb6e0512dfef7f6e97117d8c7b36e453
Aleksandr Sysoev <general@cbic-dev.com>
2024-04-22 16:49:56 +0700

    Initial commit

---
+++ lib/poolex_prom_ex.ex
@@ -0,0 +1,18 @@
+defmodule PoolexPromEx do
+  @moduledoc """
+  Documentation for `PoolexPromEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PoolexPromEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/poolex_prom_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule PoolexPromExTest do
+  use ExUnit.Case
+  doctest PoolexPromEx
+
+  test "greets the world" do
+    assert PoolexPromEx.hello() == :world
+  end
+end
