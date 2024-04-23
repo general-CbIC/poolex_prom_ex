b44036eb91477453dbf04824bd0a9226dc1f981c
Aleksandr Sysoev <general@cbic-dev.com>
2024-04-23 17:06:08 +0700

    Implement plugin (#1)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PoolexPromEx.MixProject do
     [
       app: :poolex_prom_ex,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -13,16 +13,22 @@ defmodule PoolexPromEx.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:poolex, "~> 0.9.0", optional: true},
+      {:prom_ex, "~> 1.0"},
+
+      # Development deps
+      {:credo, ">= 0.0.0", only: [:dev], runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
+      {:doctor, ">= 0.0.0", only: [:dev], runtime: false},
+      {:ex_check, "~> 0.15.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false},
+      {:makeup_diff, ">= 0.0.0", only: [:dev], runtime: false}
     ]
   end
 end
--- test/poolex_prom_ex_test.exs
+++ test/poolex_prom_ex_test.exs
@@ -1,8 +1,3 @@
 defmodule PoolexPromExTest do
   use ExUnit.Case
-  doctest PoolexPromEx
-
-  test "greets the world" do
-    assert PoolexPromEx.hello() == :world
-  end
 end
