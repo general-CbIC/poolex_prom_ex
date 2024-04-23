a864545afc71c6fef12be0e0534b6781b7cefdf3
Aleksandr Sysoev <general@cbic-dev.com>
2024-04-23 17:05:10 +0700

    Implement

--- lib/poolex_prom_ex.ex
+++ lib/poolex_prom_ex.ex
@@ -1,18 +1,63 @@
-defmodule PoolexPromEx do
+defmodule Poolex.PromEx do
   @moduledoc """
-  Documentation for `PoolexPromEx`.
+  This is a plugin for your application to collect Poolex's metrics with PromEx.
+
+  To use this plugin, you need to add `:poolex_prom_ex` to your dependencies in `mix.exs`:
+
+  ```elixir
+  defp deps do
+    [
+      {:poolex_prom_ex, "~> 0.1.0"}
+    ]
+  end
+  ```
+
+  Then you need to add this plugin to plugins list in your `PromEx` configuration:
+
+  ```elixir
+  defmodule MyCoolApp.PromEx do
+    use PromEx, otp_app: :my_cool_app
+
+    @impl PromEx
+    def plugins do
+      [
+        Poolex.PromEx
+      ]
+    end
+  end
+  ```
+
+  Additional information about `PromEx` installation and configuration can be found in the `PromEx` documentation: https://hexdocs.pm/prom_ex/readme.html#installation.
   """
+  use PromEx.Plugin
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PoolexPromEx.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @impl PromEx.Plugin
+  def event_metrics(_opts) do
+    Event.build(
+      :poolex,
+      [
+        last_value(
+          "poolex_idle_workers_count",
+          event_name: [:poolex, :metrics, :pool_size],
+          tags: [:pool_id],
+          measurement: :idle_workers_count,
+          description: "[Poolex] idle workers count"
+        ),
+        last_value(
+          "poolex_busy_workers_count",
+          event_name: [:poolex, :metrics, :pool_size],
+          tags: [:pool_id],
+          measurement: :busy_workers_count,
+          description: "[Poolex] busy workers count"
+        ),
+        last_value(
+          "poolex_is_max_overflowed",
+          event_name: [:poolex, :metrics, :pool_size],
+          tags: [:pool_id],
+          measurement: :overflowed,
+          description: "[Poolex] is pool overflowed?"
+        )
+      ]
+    )
   end
 end
--- mix.exs
+++ mix.exs
@@ -13,15 +13,22 @@ defmodule PoolexPromEx.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:prom_ex, "~> 1.0"}
+      # {:poolex, "~> 0.9.0", optional: true},
+      {:prom_ex, "~> 1.0"},
+
+      # Development deps
+      {:credo, ">= 0.0.0", only: [:dev], runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
+      {:doctor, ">= 0.0.0", only: [:dev], runtime: false},
+      {:ex_check, "~> 0.15.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false},
+      {:makeup_diff, ">= 0.0.0", only: [:dev], runtime: false}
     ]
   end
 end
